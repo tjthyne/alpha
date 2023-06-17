f5a7058c198ef40b5d586cfdb29d57a1bd89fcd8
Tim Thyne <thyne@me.com>
2023-06-17 03:03:10 -0500

    Update aud schema

--- etl/Stored Procedures/aud_cycle_initialize.sql
+++ etl/Stored Procedures/aud_cycle_initialize.sql
@@ -4,7 +4,7 @@ AS
 BEGIN
 
     set nocount on;
-    exec etl.aud_cycle_increment @source_code = @source_code
-    exec etl.aud_cycle_set @source_code = @source_code
+    exec etl.aud_cycle_increment @source_code
+    exec etl.aud_cycle_set @source_code
 
 END
