80620554b8a57a654d1bdb73234f7c5acc0fbe37
Tim Thyne <thyne@me.com>
2023-06-16 22:28:26 -0500

    Rename tables

--- dlz/Tables/membership_dlz.sql
+++ dlz/Tables/membership_dlz.sql
@@ -1,16 +1,16 @@
-CREATE TABLE [dsv].[membership] (
+CREATE TABLE [dlz].[membership_dlz] (
     [SourceCode]        VARCHAR (20)   NOT NULL,
     [SourceDescription] VARCHAR (2000) NOT NULL,
     [FirstName]         VARCHAR (200)  NULL,
     [LastName]          VARCHAR (200)  NULL,
-    [BirthDate]         VARCHAR (20)   NULL,
-    [Sex]               VARCHAR (10)   NULL,
+    [BirthDate]         DATE           NULL,
+    [Sex]               BIT            NULL,
     [PhoneNumber]       VARCHAR (10)   NULL,
     [Address1]          VARCHAR (200)  NULL,
     [Address2]          VARCHAR (200)  NULL,
     [City]              VARCHAR (200)  NULL,
-    [State]             VARCHAR (200)  NULL,
-    [ZIPCode]           VARCHAR (200)  NULL,
+    [State]             VARCHAR (2)    NULL,
+    [ZIPCode]           INT            NULL,
     [PCPName]           VARCHAR (200)  NULL,
     [PCPNPI]            VARCHAR (200)  NULL
 );
--- dsv/Tables/membership_dsv.sql
+++ dsv/Tables/membership_dsv.sql
@@ -1,16 +1,16 @@
-CREATE TABLE [dlz].[membership] (
+CREATE TABLE [dsv].[membership_dsv] (
     [SourceCode]        VARCHAR (20)   NOT NULL,
     [SourceDescription] VARCHAR (2000) NOT NULL,
     [FirstName]         VARCHAR (200)  NULL,
     [LastName]          VARCHAR (200)  NULL,
-    [BirthDate]         DATE           NULL,
-    [Sex]               BIT            NULL,
-    [PhoneNumber]       VARCHAR (10)   NULL,
+    [BirthDate]         VARCHAR (20)   NULL,
+    [Sex]               VARCHAR (10)   NULL,
+    [PhoneNumber]       VARCHAR (20)   NULL,
     [Address1]          VARCHAR (200)  NULL,
     [Address2]          VARCHAR (200)  NULL,
     [City]              VARCHAR (200)  NULL,
-    [State]             VARCHAR (2)    NULL,
-    [ZIPCode]           INT            NULL,
+    [State]             VARCHAR (200)  NULL,
+    [ZIPCode]           VARCHAR (200)  NULL,
     [PCPName]           VARCHAR (200)  NULL,
     [PCPNPI]            VARCHAR (200)  NULL
 );
